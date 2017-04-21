4c7931f0af2755b40e018a5999f233be21044ba9
NotAlexNoyle <alexnoyle@icloud.com>
2017-04-21 21:27:50 +0000

    Port SMS code to Python

---
+++ send_sms.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = "REDACTED"
+auth_token = "REDACTED"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="REDACTED",
+    from_="REDACTED",
+    body="It worked!")
+
+print("This message was sent to the user successfully: " + message.sid)
